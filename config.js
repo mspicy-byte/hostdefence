15a85853f845b6449213424daf2b49e292604157
mspicy-byte <213584076+mspicy-byte@users.noreply.github.com>
2025-06-21 19:03:25 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const antibiotics = {
+    Penicillin: { type: "Beta-lactam", strength: 30 },
+    Vancomycin: { type: "Glycopeptide", strength: 40 },
+    Azithromycin: { type: "Macrolide", strength: 25 }
+  };
+  
+  const bacteria = {
+    name: "MRSA",
+    resistance: ["Penicillin"],
+    hp: 100
+  };
+  
