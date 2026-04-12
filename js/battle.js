821897d218764cb05f677b3d44679f0d164d0083
mspicy-byte <213584076+mspicy-byte@users.noreply.github.com>
2026-04-12 20:47:05 -0300

    Update battle.js

--- js/battle.js
+++ js/battle.js
@@ -106,7 +106,6 @@ export function makeBattle(p) {
         { name: "amox_clav", display: "Amoxicillin", power: 10 },
         { name: "ampicillin", display: "Ampicillin", power: 10 },
         { name: "ceftriaxzone", display: "Ceftriaxone", power: 10 },
-        { name: "cloxacillin", display: "Cloxacillin", power: 10 },
         { name: "ciprofloxacin", display: "Ciprofloxacin", power: 10 },
         { name: "levofloxacin", display: "Levofloxacin", power: 10 },
         { name: "doxycycline", display: "Tetracycline", power: 10 },
@@ -118,14 +117,16 @@ export function makeBattle(p) {
 
         
         // Add more as per your full HTML structure:
-        // { name: "nitrofurantoin", display: "Nitrofurantoin", power: 30 },
-        // { name: "cefazolin", display: "Cefazolin", power: 40 },
-        // { name: "tmp_smx", display: "TMP-SMX", power: 50 },
-        // { name: "gentamycin", display: "Gentamycin", power: 55 },
-        // { name: "tobramycin", display: "Tobramycin", power: 55 },
-        // { name: "ertapenum", display: "Ertapenum", power: 60 },
-        // { name: "meropenum", display: "Meropenum", power: 65 },
-        // { name: "clindamycin", display: "Clindamycin", power: 45 },
+        { name: "cloxacillin", display: "Cloxacillin", power: 10 },
+         { name: "nitrofurantoin", display: "Nitrofurantoin", power: 30 },
+         { name: "cefazolin", display: "Cefazolin", power: 40 },
+         { name: "tmp_smx", display: "TMP-SMX", power: 50 },
+         { name: "gentamycin", display: "Gentamycin", power: 55 },
+         { name: "tobramycin", display: "Tobramycin", power: 55 },
+         { name: "ertapenum", display: "Ertapenum", power: 60 },
+         { name: "meropenum", display: "Meropenum", power: 65 },
+         { name: "clindamycin", display: "Clindamycin", power: 45 },
+         { name: "vancomycin", display: "Vancomycin", power: 45 },
       ],
       makeDataBox(510, 220, 38, 30, 136, 40)
     ),
